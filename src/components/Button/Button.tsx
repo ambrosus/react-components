bfaa7ef4c56089db24efa06c0c72ecb74c15ea10
namadi <nmuawiyah@gmail.com>
2019-01-14 15:39:12 +0100

    button styling and fixes

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,16 +6,17 @@ import './Button.scss';
 import { IButton } from '../../interfaces';
 
 const Button = (props: IButton) => {
-    const classes = ['AMB-Button', props.className].join(' ').trim();
+    const classes = [`AMB-Button ${props.loading && 'loading'} ${props.primary && 'primary'} ${props.outlined && 'outlined'}`, props.className].join(' ').trim();
     const disabled = props.hasOwnProperty('disabled') ? props.disabled : true;
     const spinner = props.spinner ? props.spinner : spinnerLogo;
 
-    return <button type={props.type || 'button'}
-        disabled={props.loading || disabled}
-        className={`AMB-Button ${classes} ${props.loading && 'loading'} ${props.primary && 'primary'} ${props.outlined && 'outlined'}`}>
-        {props.icon && <SVG className='Sideicon' src={props.icon} wrapper='span' />}{props.label}{props.children}
+    return (
+    <button type={props.type || 'button'} disabled={props.loading || disabled} className={classes}>
+        {props.icon && <SVG className='Sideicon' src={props.icon} wrapper='span' />}
+        {props.label}
+        {props.children}
         {props.loading && <SVG className='Loadingicon' src={spinner} />}
-    </button>;
+    </button>);
 };
 
 export default Button;
