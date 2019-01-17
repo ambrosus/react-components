b28f7c78a7901f1d8344b19001b0ed5b13fc44e6
gettingold@mail.ru <bit.size.dev@gmail.com>
2019-01-17 18:29:20 +0200

    Generate Readme.md

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,42 @@
 const path = require('path');
+const fs = require('fs');
+const pjson = require('./package.json');
+
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const CreateFileWebpack = require('create-file-webpack')
+
+function fromDir(startPath, filter, content = '') {
+
+  if (!content){
+    content = `<!---
+DO NOT EDIT THIS README.MD. IT IS GENERATED FROM COMPONENTS READMES.
+-->
+
+# React Component@${pjson.version}
+`;
+  }
+
+  const files = fs.readdirSync(startPath);
+
+  for (let i = 0; i < files.length; i++) {
+    const filename = path.join(startPath, files[i]);
+    const stat = fs.lstatSync(filename);
+    if (stat.isDirectory()) {
+      content = fromDir(filename, filter, content);
+    }
+    else if (filename.indexOf(filter) >= 0) {
+      content += fs.readFileSync(filename, 'utf8') + `
+
+      `;
+    };
+  };
+
+  return content;
+};
+
+const content = fromDir('./src', '.md');
 
 module.exports = {
   target: 'web',
@@ -9,6 +45,11 @@ module.exports = {
   plugins: [
     new CaseSensitivePathsPlugin(),
     new CleanWebpackPlugin('./dist'),
+    new CreateFileWebpack({
+      path: './',
+      fileName: 'README.MD',
+      content
+    })
   ],
   module: {
     rules: [
