d90bf77afbeb5d79830c8fa56e8aa31335fffb44
Faizan <baigfaizan95@gmail.com>
2019-01-22 18:00:52 +0530

    updated config and package.json

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,18 +1,21 @@
-const path = require('path');
-const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-  target: 'web',
-  entry: './src/index.tsx',
+  target: "web",
+  entry: "./src/index.tsx",
   devtool: "source-map",
-  mode: 'development',
+  mode: "development",
   plugins: [
+    new CleanWebpackPlugin("./docs"),
     new CaseSensitivePathsPlugin(),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html'),
-      filename: './index.html'
-    }),
+      template: path.resolve(__dirname, "public", "index.html"),
+      filename: "./index.html",
+      favicon: path.resolve(__dirname, "public", "favicons", "favicon.ico")
+    })
   ],
   module: {
     rules: [
@@ -23,47 +26,54 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-          { loader: 'sass-loader' },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
+          { loader: "style-loader" },
+          { loader: "css-loader" },
+          { loader: "sass-loader" }
         ]
       },
       {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+      },
+      {
         test: /\.svg$/,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
           name: "assets/svg/[name].[hash:8].[ext]"
         }
       },
       {
         test: /\.(png|jpg|jpeg|bmp|gif)$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           limit: 10000,
           name: "assets/images/[name].[hash:8].[ext]"
         }
-      },
-    ],
+      }
+    ]
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.scss', '.js', '.json', '.png', '.gif', '.jpg', '.svg'],
+    extensions: [
+      ".ts",
+      ".tsx",
+      ".scss",
+      ".js",
+      ".json",
+      ".png",
+      ".gif",
+      ".jpg",
+      ".svg"
+    ]
   },
   output: {
-    path: path.resolve(__dirname, 'docs/'),
-    filename: 'index.js',
+    path: path.resolve(__dirname, "docs/"),
+    filename: "index.js"
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 9000,
     open: true,
-    progress: true,
-  },
+    progress: true
+  }
 };
