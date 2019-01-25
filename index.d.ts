b4d936f52ef2c126350e331bad7af2092695c338
Lazar Eric <lazareric.com@gmail.com>
2019-01-25 10:38:26 +0100

    Textarea

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
-import { IButton, IInput, ISVG } from './src/interfaces';
+import { IButton, IInput, ISVG, ITextarea } from './src/interfaces';
 
+export function SVG(props: ISVG): any;
 export function Button(props: IButton): any;
 export function Input(props: IInput): any;
-export function SVG(props: ISVG): any;
+export function Textarea(props: ITextarea): any;
